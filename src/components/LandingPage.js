e07b9b1265716bc5ee606466a3c9746ae4942517
rkartha1 <rk82143@uga.edu>
2024-02-18 17:46:54 -0500

    created search query and search filters on landingpage.js and fixed things on landingpage.css

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,35 @@
+import React, { useState } from 'react';
 import Header from './Header';
 import './landingpage.css';
 
 function LandingPage() {
-    return (
-      <div className="App">  
-        <Header />
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchBy, setSearchBy] = useState(''); 
+
+  return (
+    <div className="App">  
+      <Header />
+      <div className="movies-and-search-container">
+        <div className="search-container">
+          <select value={searchBy} onChange={(e) => setSearchBy(e.target.value)}>
+            {searchBy === '' && <option disabled hidden value="">Search By...</option>}
+            <option value="title">Title</option>
+            <option value="genre">Genre</option>
+            <option value="date">Show Times</option>
+          </select>
+          <input
+            type="text"
+            placeholder={`Find a movie to watch!`}
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button onClick>Search</button>
+        </div>
+        <h2>Currently Running: </h2>
+        <h2>Coming Soon: </h2>
       </div>
-      
-    );
-  }
-  
-  export default LandingPage;
+    </div>
+  );
+}
+
+export default LandingPage;
