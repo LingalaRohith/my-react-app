eadbb9234ff8545a81ede816895259f0c319525a
LingalaRohith <rl92464@uga.edu>
2024-03-14 20:23:09 -0400

    Revert "Integration_login_and_signup"I revert_changes This reverts commit 91bf12ecb04f1b8850fd4c349075a1f8e1f3c5d5.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import './signup.css';
-import axios from 'axios';
 
 function Login() {
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-    });
-
-    const [popupMessage, setPopupMessage] = useState('');
-
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-      setFormData({ ...formData, [name]: value });
-  };
-
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      try{
-        const response = await axios.post('http://localhost:8080/getCustomer',formData);
-        console.log(response.data);
-        if (response.data['203']) {
-          // Incorrect password
-          setPopupMessage('Incorrect password. Please try again!');
-      } else if (response.data['204']) {
-          // User not found
-          setPopupMessage('User not found. Please sign up!');
-      } else if (response.data['200']) {
-          // Success
-          setPopupMessage('Login successful!');
-      } else {
-          // Other errors
-          setPopupMessage('An error occurred. Please try again later!');
-      }
-      }catch(error){
-        console.error('Error:', error);
-      }
-    }
-
     return (
       <div>  
         <Header />
         <div className="signup-container">
             <h3>Log in:</h3>
-            <form onSubmit={handleSubmit} className="signup-form">
-            {/* <form  action="/login" method="post" className="signup-form"> */}
+            <form action="/login" method="post" className="signup-form">
               <div className="input-group">
-                <input type="email" id="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required/>
-                <input type="password" id="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required/>
+                <input type="email" id="email" name="email" placeholder="Email" required/>
+                <input type="password" id="password" name="password" placeholder="Password" required/>
               </div>
               <div>
                 <br></br>
               </div>
-                <input type="submit" value="Submit" className="submit-button"  />
+                <input type="submit" value="Submit" className="submit-button"/>
             </form>
+<<<<<<< HEAD
             <p class="lin"><a href="/forgot-password">Forgot password?</a></p>
             {popupMessage && (
                     <div className="popup">
                         <span className="popup-message">{popupMessage}</span>
                     </div>
                 )}
+=======
+>>>>>>> parent of 91bf12e (Integration_login_and_signup)
             <p class="lin"> Don't have an account? <a href="/signup">Sign up here!</a></p>
         </div>
       </div>
